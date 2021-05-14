bb39a70b2810f38928a0c6e62364f078d8b61fc6
SmileYik <miskyle@outlook.com>
2021-05-14 18:43:12 +0800

    .

--- js/markdownEdit/markdown-edit.js
+++ js/markdownEdit/markdown-edit.js
@@ -64,3 +64,18 @@ function getCookie(key) {
 function hasCookie(key) {
     return MB_getCookie(key) !== "";
 }
+
+function downloadMd() {
+    let content = $("#mdEditArea")[0].value;
+    var eleLink = document.createElement('a');
+    eleLink.download = "markdown.md";
+    eleLink.style.display = 'none';
+    // 字符内容转变成blob地址
+    var blob = new Blob([content]);
+    eleLink.href = URL.createObjectURL(blob);
+    // 触发点击
+    document.body.appendChild(eleLink);
+    eleLink.click();
+    // 然后移除
+    document.body.removeChild(eleLink);
+};
